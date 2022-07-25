27f5e2adff71838ef6883307b0ab77ead4815103
Christian Joppi <joppi94@gmail.com>
2022-07-25 10:38:28 +0200

    Update train_pop.py

--- train_pop.py
+++ train_pop.py
@@ -29,7 +29,7 @@ def run(args):
 
     pop_signal = torch.load(args.pop_path)
 
-    train_loader = POPDataset(test_df, args.img_root, pop_signal, cat_dict, col_dict, \
+    train_loader = POPDataset(train_df, args.img_root, pop_signal, cat_dict, col_dict, \
             fab_dict, args.trend_len).get_loader(batch_size=1, train=False)
 
     test_loader = POPDataset(test_df, args.img_root, pop_signal, cat_dict, col_dict, \
